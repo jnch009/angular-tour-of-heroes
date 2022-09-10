de259d0a8911c3a030af588fb6f40d056719194c
Jeremy Ng Cheng Hin <jngchenghin@microsoft.com>
2022-09-09 17:26:58 -0700

    Hero service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from "../hero";
-import { HEROES } from "../mock-heroes";
+import { HeroService } from "../hero.service";
 
 @Component({
     selector: 'app-heroes',
@@ -8,15 +8,19 @@ import { HEROES } from "../mock-heroes";
     styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-    constructor() { }
+    constructor(private heroService: HeroService) { }
+    selectedHero?: Hero;
+    heroes: Hero[] = [];
 
     ngOnInit(): void {
+        this.getHeroes();
     }
 
-    selectedHero?: Hero;
+    getHeroes(): void {
+        this.heroes = this.heroService.getHeroes();
+    }
+
     onSelect(hero: Hero): void {
         this.selectedHero = hero;
     }
-
-    heroes = HEROES;
 }
