99c1f787a0f3321da6f42fc3b5e2bc9957530052
Jeremy Ng Cheng Hin <jngchenghin@microsoft.com>
2022-09-12 10:03:46 -0700

    Fetching heroes async

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,7 @@
 import { Hero } from "./hero";
 import { HEROES } from "./mock-heroes";
 import { Injectable } from '@angular/core';
+import { Observable, of } from "rxjs";
 
 @Injectable({
     providedIn: 'root'
@@ -9,7 +10,8 @@ export class HeroService {
 
     constructor() { }
 
-    getHeroes(): Hero[] {
-        return HEROES;
+    getHeroes(): Observable<Hero[]> {
+        const heroes = of(HEROES);
+        return heroes;
     }
 }
