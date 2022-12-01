8a36db216dca24634b0ed1b69a3fe1c166d22622
Jeremy Ng Cheng Hin <jngchenghin@microsoft.com>
2022-11-30 16:50:11 -0800

    Adding a new hero

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -65,4 +65,11 @@ export class HeroService {
             catchError(this.handleError<Hero>(`updateHero`))
         )
     }
+
+    addHero(hero: Hero): Observable<Hero> {
+        return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+            tap((newHero: Hero) => this.log(`added hero with id=${newHero.id}`)),
+            catchError(this.handleError<Hero>(`addHero`))
+        )
+    }
 }
